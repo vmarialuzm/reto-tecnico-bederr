c8168e4a0e9724ff4bd58af816a52e3dc0498800
vmarialuzm <vmarialuzm@gmail.com>
2024-12-17 12:19:48 -0500

    initial config front

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">
+        ¡Hola, React + TypeScript + Tailwind!
+      </h1>
+    </div>
+  );
+}
+
+export default App
